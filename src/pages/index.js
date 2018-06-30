7efb14ab6827fff8ae6159f08e8f249407ead61c
Tim Normington <tim.j.normington@gmail.com>
2018-06-30 09:06:24 -0600

    Fixed apostrophes in content

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,10 @@ const IndexPage = () => (
         <div id="anchor-link" />
         <p>
           Multiple different forms of media were used in creating your regularly scheduled content.
-  We're sticklers for quality ova here, and that's why nothing will appear under our flag unless it has
+  We&#39;re sticklers for quality ova here, and that's why nothing will appear under our flag unless it has
   well-founded and legitimate merit.  Our goal is to spread the good word, inspire others to express themselves, 
   and to be a voice of legitimacy in a time where those are hard  to come by. We enjoy a good time. You to doo. 
-  We're in this together, friend. Check it awt.
+  We&#39;re in this together, friend. Check it awt.
         </p>
         <h2>Interested?</h2>
         <NewsletterForm />
